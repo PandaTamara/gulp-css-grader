5f83227078aa7dd80e88c6d7342a97452c86be2b
panda_tamara <lokon38@gmail.com>
2019-05-06 21:05:48 +0300

    init fish

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gutil           = require('gulp-util'),
+    Transform       = require('stream').Transform,
+    PluginError     = gutil.PluginError,
+    through         = require('through2');
+
+var PLUGIN_NAME = 'gulp-css-grader';
+
+
+module.exports = function() {
+    return through.obj(function(file, encoding, callback) {
+        if (file.isNull()) {
+            // nothing to do
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            // file.contents is a Stream - https://nodejs.org/api/stream.html
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+
+            // or, if you can handle Streams:
+            //file.contents = file.contents.pipe(...
+            //return callback(null, file);
+        } else if (file.isBuffer()) {
+            // file.contents is a Buffer - https://nodejs.org/api/buffer.html
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+
+            // or, if you can handle Buffers:
+            //file.contents = ...
+            //return callback(null, file);
+        }
+    });
+};
+
+
+//module.exports = gulpcssoverrides;
